734bf78b4f71b59adf7a972215cd58f72d9c06c4
chand1997 <chanduchallahalli@gmail.com>
2023-07-11 10:52:30 +0530

    calling meth method of class A

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,18 @@
 package com.driver;
 
 public class Main {
+     public static class A{
+        public String meth(){
+            return "Invoking method from class A";
+        }
+    }
+      public static class B extends A{
+
+    }
+
+    public static void main(String[] args) {
+        B objB=new B();
+        objB.meth();
+    }
   
 }
